93ce5bc25a07e35a8e2f4e77ae8566b957eaa36f
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-12 11:24:50 +0530

    add set.py

---
+++ week2/set.py
@@ -0,0 +1,24 @@
+# create empty set
+
+s = set()
+
+s.add("name")
+s.add(2)
+s.add(13)
+s.add("23")
+s.add("sham")
+s.add("sham")
+s.add(30)
+
+s.add(13)
+
+
+print(s)
+
+#remove 
+s.remove("sham")
+
+print(s)
+
+#find length of set
+print(len(s))
