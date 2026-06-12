cf3934bc48b78e5c9d4f2ecd73b13c8f49e18a00
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-12 11:40:20 +0530

    add dictionaries.py

---
+++ week2/dictionaries.py
@@ -0,0 +1,12 @@
+houses = {"ram":"surat",
+          "jay": "vapi"}
+
+print(houses["jay"])
+print(houses)
+#add value
+houses["sham"] = "silavassa"
+
+#change value
+houses["ram"] = "jalgaon"
+
+print(houses)
