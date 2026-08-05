4bc6c12fcc1a1873ea0bfaba787c56bb2d0e9f89
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-08-05 23:12:23 +0530

    learn session storage

--- week3/lecture3/tasks/views.py
+++ week3/lecture3/tasks/views.py
@@ -1,17 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
+from django.urls import reverse
 
 # Create your views here.
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
-tasks = ["foo", "bar", "now"]
+tasks = []
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, 'tasks/index.html',{
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            priority = form.cleaned_data["priority"]
+            request.session["tasks"] += [f"{task} - {priority}"]
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, 'tasks/add.html',{
+                "form": form
+            })
     return render(request, 'tasks/add.html',{
         "form": NewTaskForm()
     })
